d476bb2052efce565f61e6260c08922421e98373
Tomas Ruiz <tomastrg22@gmail.com>
2017-04-10 18:42:56 +0200

    stack and stack test complete

---
+++ test/test.js
@@ -0,0 +1,54 @@
+/*jshint esversion:6*/
+const expect = require('chai').expect;
+const stack = require('../index');
+
+describe('Stack', () => {
+  it('should return size 1', () => {
+    let result = stack(1);
+    expect(result.size).to.equal(1);
+  });
+
+  it('should return size 5', () => {
+    let result = stack(5);
+    expect(result.size).to.equal(5);
+  });
+
+  it('should return size 10', () => {
+    let result = stack(10);
+    expect(result.size).to.equal(10);
+  });
+
+  it('should return isEmpty true', () => {
+    let stck = stack(0);
+    let result = stck.isEmpty();
+    expect(result).to.equal(true);
+  });
+
+  it('should return isEmpty false', () => {
+    let stck = stack(3);
+    stck.push(1);
+    let result = stck.isEmpty();
+    expect(result).to.equal(false);
+  });
+
+  it('should return isFull false', () => {
+    let stck = stack(3);
+    stck.push(1);
+    let result = stck.isFull();
+    expect(result).to.equal(false);
+  });
+  it('should return isFull true', () => {
+    let stck = stack(1);
+    stck.push(1);
+    let result = stck.isFull();
+    expect(result).to.equal(true);
+  });
+
+  // it('should push(1) return [1]', () => {
+  //   let stck = stack(3);
+  //   let result = stck.push(1);
+  //   expect(result).to.equal([1]);
+  // });
+
+
+});
